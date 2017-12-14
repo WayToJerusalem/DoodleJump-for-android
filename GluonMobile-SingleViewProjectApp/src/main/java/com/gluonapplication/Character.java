01daf13ed839950bc7530873ceabe3f055164af1
Dima <dimatsuman90@gmail.com>
2017-12-14 20:22:40 +0200

    update

--- GluonMobile-SingleViewProjectApp/src/main/java/com/gluonapplication/Character.java
+++ GluonMobile-SingleViewProjectApp/src/main/java/com/gluonapplication/Character.java
@@ -65,7 +65,6 @@ public class Character extends Pane{
             playerVelocity = playerVelocity.add(0,-30);
             canJump = false;
         }
-
     }
 
     public void setCanJump(boolean canJump) {
